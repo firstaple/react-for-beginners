c61a647185ac59ef7d7eb2fac99156411d2505e0
firstaple <firstaple@gmail.com>
2022-12-14 23:26:01 +0900

    src

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function App() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}></Route>
+        <Route path="/movie/:id" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
